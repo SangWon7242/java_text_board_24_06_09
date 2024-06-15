52441a860869c3c256354392d37d68cd6fda59b1
SangWon7242 <gotkdqja@gmail.com>
2024-06-15 13:15:47 +0900

    테스트 게시물 생성 함수 적용

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -5,18 +5,21 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
+  static void makeTestData(List<Article> articles) {
+    articles.add(new Article(1, "제목1", "내용1"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
+  }
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int articleLastId = 0;
     Article lastArticle = null;
     
     List<Article> articles = new ArrayList<>();
-    
-    // 테스트 게시물 시작
-    articles.add(new Article(1, "제목1", "내용1"));
-    articles.add(new Article(2, "제목2", "내용2"));
-    articles.add(new Article(3, "제목3", "내용3"));
-    // 테스트 게시물 끝
+
+    makeTestData(articles);
 
     System.out.println("== 자바 텍스트 게시판 ==");
 
