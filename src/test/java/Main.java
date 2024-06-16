12c70fbdbf97a40996807922505342e0320e67fa
SangWon7242 <gotkdqja@gmail.com>
2024-06-16 13:41:55 +0900

    Util 클래스 도입, 파싱로직을 별도의 함수로 만들어서 필요할 때마다 호출

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,38 +2,28 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    // 압축 된 데이터
     String queryString1 = "id=33&hitCount=1&age=14&studentNo=65&name=Bob";
-    
-    // 압축 해제 시작
-    String[] queryStringBits1 = queryString1.split("&");
+    Map<String, String> params1 = Util.getParams(queryString1);
+    System.out.println(params1);
 
-    Map<String, String> params1 = new HashMap<>();
+    String queryString2 = "id=15&hitCount=100";
+    Map<String, String> params2 = Util.getParams(queryString2);
+    System.out.println(params2);
+  }
+}
+
+class Util {
+  static Map<String, String> getParams(String queryStr) {
+    Map<String, String> params = new HashMap<>();
+
+    String[] queryStringBits1 = queryStr.split("&");
 
     for(String bit : queryStringBits1) {
       String[] bitBits = bit.split("=");
 
-      params1.put(bitBits[0], bitBits[1]);
+      params.put(bitBits[0], bitBits[1]);
     }
-    // 압축 해제 끝
 
-    System.out.println(params1);
-
-    // 압축 된 데이터
-    String queryString2 = "id=33&hitCount=1";
-
-    // 압축 해제 시작
-    String[] queryStringBits2 = queryString2.split("&");
-
-    Map<String, String> params2 = new HashMap<>();
-
-    for(String bit : queryStringBits2) {
-      String[] bitBits = bit.split("=");
-
-      params2.put(bitBits[0], bitBits[1]);
-    }
-    // 압축 해제 끝
-
-    System.out.println(params2);
+    return params;
   }
 }
