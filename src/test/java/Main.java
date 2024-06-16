3eb23630594d3ac6a3e04b5be4096cefb70782c9
SangWon7242 <gotkdqja@gmail.com>
2024-06-16 12:39:22 +0900

    List를 도입하여 파라미터 데이터를 유연하게 다루도록 로직 수정

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,36 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    String queryString = "a=100&b=20&c=30&d=40&e=50";
+    String queryString = "b=20&a=100&c=30&d=40&e=50";
     String[] queryStringBits = queryString.split("&");
 
-    // 문제점 : 로직 자체 유연하지 못한다.
-    // 왜? 파라미터가 추가되면 그거에 맞는 로직을 다시 수정해줘야 하기 때문에
-
-    int a = 0;
-    int b = 0;
-    int c = 0;
-    int d = 0;
+    List<String> paramNames = new ArrayList<>();
+    List<Integer> paramValues = new ArrayList<>();
 
     for(String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
 
-      String paramName = bitBits[0];
-      String paramValue = bitBits[1];
-
-      if(paramName.equals("a")) {
-        a = Integer.parseInt(paramValue);
-      } else if (paramName.equals("b")) {
-        b = Integer.parseInt(paramValue);
-      } else if (paramName.equals("c")) {
-        c = Integer.parseInt(paramValue);
-      } else if (paramName.equals("d")) {
-        d = Integer.parseInt(paramValue);
-      }
+      paramNames.add(bitBits[0]);
+      paramValues.add(Integer.parseInt(bitBits[1]));
     }
 
-    System.out.printf("a : %d\n", a);
-    System.out.printf("b : %d\n", b);
-    System.out.printf("c : %d\n", c);
-    System.out.printf("d : %d\n", d);
+    for(int i = 0; i < paramNames.size(); i++) {
+      String paramName = paramNames.get(i);
+      int paramValue = paramValues.get(i);
+
+      System.out.printf("%s : %d\n", paramName, paramValue);
+    }
   }
 }
