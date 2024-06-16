15fab98bdbece11277f3049bfc03ca428d36c59b
SangWon7242 <gotkdqja@gmail.com>
2024-06-16 12:20:24 +0900

    현재 로직의 문제점 파악

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,11 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    String queryString = "a=100&b=20&c=30";
+    String queryString = "a=100&b=20&c=30&d=40&e=50";
     String[] queryStringBits = queryString.split("&");
+
+    // 문제점 : 로직 자체 유연하지 못한다.
+    // 왜? 파라미터가 추가되면 그거에 맞는 로직을 다시 수정해줘야 하기 때문에
 
     int a = 0;
     int b = 0;
     int c = 0;
+    int d = 0;
 
     for(String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
@@ -19,11 +23,14 @@ public class Main {
         b = Integer.parseInt(paramValue);
       } else if (paramName.equals("c")) {
         c = Integer.parseInt(paramValue);
+      } else if (paramName.equals("d")) {
+        d = Integer.parseInt(paramValue);
       }
     }
 
     System.out.printf("a : %d\n", a);
     System.out.printf("b : %d\n", b);
     System.out.printf("c : %d\n", c);
+    System.out.printf("d : %d\n", d);
   }
 }
