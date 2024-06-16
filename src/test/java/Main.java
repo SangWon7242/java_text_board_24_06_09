a708117bebd7c9f1a903444c00c346bab230bd58
SangWon7242 <gotkdqja@gmail.com>
2024-06-16 12:18:12 +0900

    뽑아낸 문자열 데이터를 Integer.parseInt를 활용하여 정수로 형변환

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    String queryString = "a=1&b=2&c=3";
+    String queryString = "a=100&b=20&c=30";
     String[] queryStringBits = queryString.split("&");
+
+    int a = 0;
+    int b = 0;
+    int c = 0;
 
     for(String bit : queryStringBits) {
       String[] bitBits = bit.split("=");
@@ -9,7 +13,17 @@ public class Main {
       String paramName = bitBits[0];
       String paramValue = bitBits[1];
 
-      System.out.printf("%s : %s\n", paramName, paramValue);
+      if(paramName.equals("a")) {
+        a = Integer.parseInt(paramValue);
+      } else if (paramName.equals("b")) {
+        b = Integer.parseInt(paramValue);
+      } else if (paramName.equals("c")) {
+        c = Integer.parseInt(paramValue);
+      }
     }
+
+    System.out.printf("a : %d\n", a);
+    System.out.printf("b : %d\n", b);
+    System.out.printf("c : %d\n", c);
   }
 }
