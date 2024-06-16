759285ec132f8db1da72a815f5dc1b3fda972f09
SangWon7242 <gotkdqja@gmail.com>
2024-06-16 13:51:10 +0900

    Util.getParams 함수를 오류기반개발로 구현 및 개선

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,13 +2,11 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    String queryString1 = "id=33&hitCount=1&age=14&studentNo=65&name=Bob";
-    Map<String, String> params1 = Util.getParams(queryString1);
-    System.out.println(params1);
-
-    String queryString2 = "id=15&hitCount=100";
-    Map<String, String> params2 = Util.getParams(queryString2);
-    System.out.println(params2);
+    Map<String, String> params = Util.getParams("id=33&hitCount=1&name=Bob&calc=[1=2]");
+    System.out.println(params);
+    System.out.println(params.get("id")); // 33
+    System.out.println(params.get("name")); // Bob
+    System.out.println(params.get("calc"));
   }
 }
 
@@ -16,12 +14,14 @@ class Util {
   static Map<String, String> getParams(String queryStr) {
     Map<String, String> params = new HashMap<>();
 
-    String[] queryStringBits1 = queryStr.split("&");
+    for(String bit : queryStr.split("&")) {
+      String[] bits = bit.split("=", 2);
 
-    for(String bit : queryStringBits1) {
-      String[] bitBits = bit.split("=");
+      if(bits.length == 1) {
+        continue;
+      }
 
-      params.put(bitBits[0], bitBits[1]);
+      params.put(bits[0], bits[1]);
     }
 
     return params;
