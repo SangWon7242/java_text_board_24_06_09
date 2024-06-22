b01319369ae73cd3f1887b01e80e2f5c6ef06421
SangWon7242 <gotkdqja@gmail.com>
2024-06-22 13:07:13 +0900

    게시물 상세 보기 시에, 원하는 게시물 번호 입력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -29,6 +29,7 @@ public class Main {
       String cmd = sc.nextLine();
 
       Rq rq = new Rq(cmd);
+      Map<String, String> params = rq.getParams();
 
       if (rq.getUrlPath().equals("/usr/article/write")) {
         System.out.print("제목 : ");
@@ -55,12 +56,19 @@ public class Main {
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       } else if (rq.getUrlPath().equals("/usr/article/detail")) {
+        int id = Integer.parseInt(params.get("id"));
+
         if (articles.isEmpty()) {
           System.out.println("게시물이 존재하지 않습니다.");
           continue;
         }
 
-        Article article = articles.get(articles.size() - 1);
+        if(id > articles.size()) {
+          System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+          continue;
+        }
+
+        Article article = articles.get(id - 1);
 
         System.out.println("== 게시물 상세보기 ==");
         System.out.printf("번호 : %d\n", article.id);
