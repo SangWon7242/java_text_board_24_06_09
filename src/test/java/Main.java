29c9bb0082abae15dadb1683b86864153814c9d6
SangWon7242 <gotkdqja@gmail.com>
2024-06-22 12:20:07 +0900

    Rq 객체를 만들어서, 원하는 데이터 가져오기

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,21 +2,38 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    Map<String, String> params = Util.getParamsFromUrl("/usr/article/list?page=2&searchKeyword=안녕? 나는");
+    Rq rq = new Rq("/usr/article/list?page=2&searchKeyword=안녕? 나는");
+
+    Map<String, String> params = rq.getParams();
     System.out.println(params);
-    System.out.println(params.get("page")); // 2
-    System.out.println(params.get("searchKeyword")); // 안녕
 
-    String urlPath = Util.getUrlPathFromUrl("/usr/article/list?page=2&searchKeyword=안녕? 나는");
-    System.out.println(urlPath);
+    params = rq.getParams();
+    System.out.println(params);
 
-    urlPath = Util.getUrlPathFromUrl("/usr/article/list");
+    String urlPath = rq.getUrlPath();
     System.out.println(urlPath);
+  }
+}
+
+class Rq {
+  String url;
+
+  Rq(String url) {
+    this.url = url;
+  }
+
+  public Map<String, String> getParams() {
+    return Util.getParamsFromUrl(url);
+  }
+
+  public String getUrlPath() {
+    return Util.getUrlPathFromUrl(url);
   }
 }
 
 class Util {
   static Map<String, String> getParamsFromUrl(String url) {
+    System.out.println("getParamsFromUrl 실행");
     Map<String, String> params = new HashMap<>();
     String[] urlBits = url.split("\\?", 2);
 
@@ -40,6 +57,7 @@ class Util {
   }
 
   static String getUrlPathFromUrl(String url) {
+    System.out.println("getUrlPathFromUrl 실행");
     return url.split("\\?", 2)[0];
   }
 }
