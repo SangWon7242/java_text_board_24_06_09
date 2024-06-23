49e01996a5a2da395045a18eca1b254d3aa520cb
SangWon7242 <gotkdqja@gmail.com>
2024-06-23 13:40:42 +0900

    게시물 수정 기능 구현

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -37,7 +37,9 @@ public class Main {
         actionUsrArticleDetail(rq, articles);
       } else if (rq.getUrlPath().equals("/usr/article/list")) {
         actionUsrArticleList(rq, articles);
-      } else if (cmd.equals("exit")) {
+      } else if (rq.getUrlPath().equals("/usr/article/modify")) {
+        actionUsrArticleModify(sc, rq, articles);
+      }  else if (cmd.equals("exit")) {
         System.out.println("== 자바 텍스트 게시판 종료 ==");
         break;
       } else {
@@ -46,6 +48,43 @@ public class Main {
     }
 
     sc.close();
+  }
+
+  private static void actionUsrArticleModify(Scanner sc, Rq rq, List<Article> articles) {
+    Map<String, String> params = rq.getParams();
+
+    if(!params.containsKey("id")) {
+      System.out.println("id를 입력해주세요.");
+      return;
+    }
+
+    int id = 0;
+
+    try {
+      id = Integer.parseInt(params.get("id"));
+    } catch (NumberFormatException e) {
+      System.out.println("id를 정수 형태로 입력해주세요.");
+      return;
+    }
+
+    if (articles.isEmpty()) {
+      System.out.println("게시물이 존재하지 않습니다.");
+      return;
+    }
+
+    if(id > articles.size()) {
+      System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+      return;
+    }
+
+    Article article = articles.get(id - 1);
+
+    System.out.print("새 내용 : ");
+    article.title = sc.nextLine();
+    System.out.print("새 제목 : ");
+    article.content = sc.nextLine();
+
+    System.out.printf("%d번 게시물을 수정하였습니다.\n", id);
   }
 
   private static void actionUsrArticleWrite(Scanner sc, int articleLastId, List<Article> articles) {
