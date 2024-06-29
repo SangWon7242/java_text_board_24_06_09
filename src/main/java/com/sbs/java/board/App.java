864388b57c492aee807e932be05d2fc138210840
SangWon7242 <gotkdqja@gmail.com>
2024-06-29 12:12:45 +0900

    App 클래스 도입, static 대거 제거

---
+++ src/main/java/com/sbs/java/board/App.java
@@ -0,0 +1,242 @@
+package com.sbs.java.board;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+public class App {
+  int articleLastId;
+  List<Article> articles;
+
+  public App() {
+    articleLastId = 0;
+    articles = new ArrayList<>();
+  }
+
+  void makeTestData() {
+    IntStream.rangeClosed(1, 100)
+        .forEach(i -> articles.add(new Article(i, "제목" + i, "내용" + i)));
+  }
+
+  public void run() {
+    makeTestData();
+
+    if (!articles.isEmpty()) {
+      articleLastId = articles.get(articles.size() - 1).id;
+    }
+
+    System.out.println("== 자바 텍스트 게시판 ==");
+
+    while (true) {
+      System.out.print("명령) ");
+      String cmd = Container.sc.nextLine();
+
+      Rq rq = new Rq(cmd);
+
+      if (rq.getUrlPath().equals("/usr/article/write")) {
+        actionUsrArticleWrite();
+      } else if (rq.getUrlPath().equals("/usr/article/detail")) {
+        actionUsrArticleDetail(rq);
+      } else if (rq.getUrlPath().equals("/usr/article/list")) {
+        actionUsrArticleList(rq);
+      } else if (rq.getUrlPath().equals("/usr/article/modify")) {
+        actionUsrArticleModify(rq);
+      } else if (rq.getUrlPath().equals("/usr/article/delete")) {
+        actionUsrArticleDelete(rq);
+      } else if (cmd.equals("exit")) {
+        System.out.println("== 자바 텍스트 게시판 종료 ==");
+        break;
+      } else {
+        System.out.println("올바른 명령어가 아닙니다.");
+      }
+    }
+
+    Container.sc.close();
+  }
+
+  private void actionUsrArticleDelete(Rq rq) {
+    Map<String, String> params = rq.getParams();
+
+    if (!params.containsKey("id")) {
+      System.out.println("id를 입력해주세요.");
+      return;
+    }
+
+    int id = 0;
+
+    try {
+      id = Integer.parseInt(params.get("id"));
+    } catch (NumberFormatException e) {
+      System.out.println("id를 정수 형태로 입력해주세요.");
+      return;
+    }
+
+    if (articles.isEmpty()) {
+      System.out.println("게시물이 존재하지 않습니다.");
+      return;
+    }
+
+    Article article = articleFindById(id, articles);
+
+    if (article == null) {
+      System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+      return;
+    }
+
+    articles.remove(article);
+
+    System.out.printf("%d번 게시물이 삭제되었습니다.\n", id);
+  }
+
+  private void actionUsrArticleModify(Rq rq) {
+    Map<String, String> params = rq.getParams();
+
+    if (!params.containsKey("id")) {
+      System.out.println("id를 입력해주세요.");
+      return;
+    }
+
+    int id = 0;
+
+    try {
+      id = Integer.parseInt(params.get("id"));
+    } catch (NumberFormatException e) {
+      System.out.println("id를 정수 형태로 입력해주세요.");
+      return;
+    }
+
+    if (articles.isEmpty()) {
+      System.out.println("게시물이 존재하지 않습니다.");
+      return;
+    }
+
+    Article article = articleFindById(id, articles);
+
+    if (article == null) {
+      System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+      return;
+    }
+
+    System.out.print("새 내용 : ");
+    article.title = Container.sc.nextLine();
+    System.out.print("새 제목 : ");
+    article.content = Container.sc.nextLine();
+
+    System.out.printf("%d번 게시물을 수정하였습니다.\n", id);
+  }
+
+  private void actionUsrArticleWrite() {
+    System.out.print("제목 : ");
+    String title = Container.sc.nextLine();
+
+    if (title.trim().isEmpty()) {
+      System.out.println("제목을 입력해주세요.");
+      return;
+    }
+
+    System.out.print("내용 : ");
+    String content = Container.sc.nextLine();
+
+    if (content.trim().isEmpty()) {
+      System.out.println("내용을 입력해주세요.");
+      return;
+    }
+
+    int id = ++articleLastId;
+
+    Article article = new Article(id, title, content);
+
+    articles.add(article);
+
+    System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
+  }
+
+  private void actionUsrArticleDetail(Rq rq) {
+    Map<String, String> params = rq.getParams();
+
+    if (!params.containsKey("id")) {
+      System.out.println("id를 입력해주세요.");
+      return;
+    }
+
+    int id = 0;
+
+    try {
+      id = Integer.parseInt(params.get("id"));
+    } catch (NumberFormatException e) {
+      System.out.println("id를 정수 형태로 입력해주세요.");
+      return;
+    }
+
+    if (articles.isEmpty()) {
+      System.out.println("게시물이 존재하지 않습니다.");
+      return;
+    }
+
+    Article article = articleFindById(id, articles);
+
+    if (article == null) {
+      System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+      return;
+    }
+
+    System.out.println("== 게시물 상세보기 ==");
+    System.out.printf("번호 : %d\n", article.id);
+    System.out.printf("제목 : %s\n", article.title);
+    System.out.printf("내용 : %s\n", article.content);
+  }
+
+  private void actionUsrArticleList(Rq rq) {
+    Map<String, String> params = rq.getParams();
+
+    System.out.println("== 게시물 리스트 ==");
+    System.out.println("번호 | 제목");
+
+    boolean orderByIdDesc = true; // 역순 정렬에 대한 코드
+
+    if (params.containsKey("orderBy") && params.get("orderBy").equals("idAsc")) {
+      orderByIdDesc = false;
+    }
+
+    // 검색 시작
+    // articles : 정렬 되지 않은 1 ~ 100개의 게시물
+    List<Article> filteredArticles = articles;
+
+    if (params.containsKey("searchKeyword")) {
+      String searchKeyword = params.get("searchKeyword");
+
+      filteredArticles = new ArrayList<>();
+
+      for (Article article : articles) {
+        if (article.title.contains(searchKeyword) || article.content.contains(searchKeyword)) {
+          filteredArticles.add(article);
+        }
+      }
+    }
+    // 검색 끝
+
+    // 정렬 시작
+    List<Article> sortedArticles = filteredArticles;
+
+    if (orderByIdDesc) {
+      sortedArticles = Util.reverseList(sortedArticles);
+    }
+    // 정렬 끝
+
+    // 게시물 리스트 출력
+    for (Article article : sortedArticles) {
+      System.out.printf("%d | %s\n", article.id, article.title);
+    }
+  }
+
+  private Article articleFindById(int id, List<Article> articles) {
+    for (Article article : articles) {
+      if (article.id == id) {
+        return article;
+      }
+    }
+
+    return null;
+  }
+}
